351ec6970eb447dd843555d1068d24c213cb1647
Grudger <aamir00@hotmail.com>
2015-11-07 18:02:31 +0500

    Installation Complete

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/admin/');
+define('HTTP_CATALOG', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/admin/');
+define('HTTPS_CATALOG', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/wamp/www/admin/');
+define('DIR_SYSTEM', 'F:/wamp/www/system/');
+define('DIR_LANGUAGE', 'F:/wamp/www/admin/language/');
+define('DIR_TEMPLATE', 'F:/wamp/www/admin/view/template/');
+define('DIR_CONFIG', 'F:/wamp/www/system/config/');
+define('DIR_IMAGE', 'F:/wamp/www/image/');
+define('DIR_CACHE', 'F:/wamp/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/wamp/www/system/storage/download/');
+define('DIR_LOGS', 'F:/wamp/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/wamp/www/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/wamp/www/system/storage/upload/');
+define('DIR_CATALOG', 'F:/wamp/www/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mysql');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'F:/wamp/www/catalog/');
+define('DIR_SYSTEM', 'F:/wamp/www/system/');
+define('DIR_LANGUAGE', 'F:/wamp/www/catalog/language/');
+define('DIR_TEMPLATE', 'F:/wamp/www/catalog/view/theme/');
+define('DIR_CONFIG', 'F:/wamp/www/system/config/');
+define('DIR_IMAGE', 'F:/wamp/www/image/');
+define('DIR_CACHE', 'F:/wamp/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'F:/wamp/www/system/storage/download/');
+define('DIR_LOGS', 'F:/wamp/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'F:/wamp/www/system/storage/modification/');
+define('DIR_UPLOAD', 'F:/wamp/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'mysql');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
